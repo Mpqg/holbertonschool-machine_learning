8295bb0ca4caa9398de046a8b61f711f986a18e7
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-07-30 23:39:53 +0000

    Fixing documentation

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-
-
-import numpy as np
+"""Cats got your tongue"""
 
 
 def np_cat(mat1, mat2, axis=0):
-    """
-    Concatenate two matrices along a specific axis and
-    return the result as a new numpy.ndarray.
-    """
+    """Concatenate two matrices along a specific axis and
+    return the result as a new numpy.ndarray."""
     return np.concatenate((mat1, mat2), axis=axis)
