9d15be5e4f5b318935d727d12562aa3f750c5a32
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-07-30 23:35:19 +0000

    Fixing documentation

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-
-
-import numpy as np
+"""The western exchange"""
 
 
 def np_transpose(matrix):
-    """
-    Transpose a numpy.ndarray and return
-    the transposed array as a new numpy.ndarray.
-    """
+    """Transpose a numpy.ndarray and return
+    the transposed array as a new numpy.ndarray."""
     return np.transpose(matrix)
