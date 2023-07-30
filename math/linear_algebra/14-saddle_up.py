bedbb8e1e8d4195e734c3748f1eb8a07014cbc0a
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-07-30 23:41:50 +0000

    Fixing documentation

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-
-
+"""Saddle Up"""
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """
-    Perform matrix multiplication on two numpy.ndarrays
-    and return the result as a new numpy.ndarray.
-    """
+    """Perform matrix multiplication on two numpy.ndarrays
+    and return the result as a new numpy.ndarray."""
     return np.dot(mat1, mat2)
