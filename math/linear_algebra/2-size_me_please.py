c252ccb43bca72b5044f7a2e59a30fc6d196b334
Mpqg <mapazquiga@gmail.com>
2023-07-30 16:36:27 +0000

    adding doc of matrix2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
-def matrix_shape(matrix):
-"""OK"""
+  def matrix_shape(matrix):
+    """
+    Calculate the shape of a matrix.
+    Args:
+        matrix (list): The input matrix.
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
