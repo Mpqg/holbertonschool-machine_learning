212da80df7daa3d54f4c507b53e31e92ab655763
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-07-30 23:30:13 +0000

    Fixing documentation

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
+"""Howdy partner"""
 
 
 def cat_arrays(arr1, arr2):
-    """
-    Concatenate two arrays and return the result as a new list.
-    """
+    """Concatenate two arrays and return the result as a new list."""
     return arr1 + arr2
