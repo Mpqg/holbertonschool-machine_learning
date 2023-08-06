fc5e8d57dcd5a4f66b2842d46b538a17970b3d85
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-08-06 18:08:56 +0000

    exe 9 update

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,10 +2,14 @@
 """Summation"""
 
 
+import sys
+
+
 def summation_i_squared(n):
     """Summation"""
+    recLimit = sys.getrecursionlimit()
     if not isinstance(n, int) or n <= 0:
         return None
-    if n == 1:
+    if n == 1 or n <= recLimit:
         return 1
     return n ** 2 + summation_i_squared(n - 1)
