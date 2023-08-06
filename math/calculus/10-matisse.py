da707529ac4b103463d89bd3bf6e83b7a1961503
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-08-06 08:00:23 +0000

    exec 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,11 +4,9 @@
 
 def poly_derivative(poly):
     """poly"""
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float))
-                                             for coeff in poly):
+    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly):
         return None
-    derivative = [coeff * power for power,
-                  coeff in enumerate(poly[1:], start=1)]
+    derivative = [coeff * power for power, coeff in enumerate(poly[1:], start=1)]
     if all(deriv_coeff == 0 for deriv_coeff in derivative):
         return [0]
     return derivative
