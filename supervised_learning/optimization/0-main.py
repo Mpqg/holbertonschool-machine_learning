b23591c69f18b04e0f4108abbb6b3995fde90c01
María Paz Quirola <98773244+Mpqg@users.noreply.github.com>
2023-08-31 03:16:48 +0000

    upd 0

--- supervised_learning/optimization/0-main.py
+++ supervised_learning/optimization/0-main.py
@@ -12,4 +12,3 @@ if __name__ == '__main__':
     m, s = normalization_constants(X)
     print(m)
     print(s)
-    
--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,9 +1,8 @@
+#!/usr/bin/env python3
+""" normalize"""
 import numpy as np
 
 
 def normalization_constants(X):
-    """Calculate the mean and standard deviation."""
-    # Calculate the mean and standard deviation along axis 0 (columns)
-    means = np.mean(X, axis=0)
-    std_devs = np.std(X, axis=0)
-    return means, std_devs
+    """ normalization function"""
+    return np.mean(X, axis=0), np.std(X, axis=0)
